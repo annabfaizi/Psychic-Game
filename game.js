cdd02cb3e8fb60db5cded884d520bfc54ccc9128
Anna B Faizi <annanoor.b@gmail.com>
2017-04-29 03:14:53 -0700

    updated js files

--- game.js
+++ game.js
@@ -2,12 +2,25 @@
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //pc letter selection
-var pcChoice = letters.random();
+var pcChoice = letters[Math.floor(Math.random()* letters.length)];
+console.log(pcChoice);
 
 //keeping track
 var wins = 0;
 var losses = 0;
-var guesses = 10;
+var guesses = 9;
+var guessesLeft = 9;
+
+//resets everything
+var reset = function() {
+	guesses = 9;
+	guessesLeft = 9;
+	guessedLetters = [];
+
+	updateguessesLeft();
+	updatepressed();
+
+}
 
 //logic for user key press
 document.onkeypress = function(e){
@@ -15,12 +28,19 @@ document.onkeypress = function(e){
 	var pressed;
 	
 	pressed = e.key;
+	//comparing letters
+	var compare = function(pressed, pcChoice){
+		if(pressed===pcChoice){
+			wins++;
+		}
 
-	if (guesses > 0 && pressed !== pcChoice){
-		losses--;
-	}
-	else {
-		wins++;
+		else if (guesses === 0){
+			reset();
+		}
+
+		else {
+			losses++;
+		}
 	}
 }
 
@@ -28,6 +48,5 @@ document.onkeypress = function(e){
 
 
 
-//comparing letters
-var match = compare(pressed, pcChoice);
+
 
