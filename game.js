74f6ce24c92a127dfcb230566a472646fa7096de
Anna B Faizi <annanoor.b@gmail.com>
2017-04-29 13:32:01 -0700

    updated js files

--- game.js
+++ game.js
@@ -4,45 +4,50 @@ $( document ).ready(function() {
 
 //set of letters
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+var pressedLetters = [];
 //pc letter selection
 var pcChoice = letters[Math.floor(Math.random()* letters.length)];
 	console.log(pcChoice);
 
-//keeping track
-var wins = 0;
-var losses = 0;
-var guessesLeft = 9;
+//logic for user key press
+document.onkeypress = function(e){
+	//user key press
+	var pressed;
+	pressed = e.key;
+	console.log(pressed);
+	pressedLetters.join(pressed + (", "));
+	document.getElementById("pressedLetters").innerHTML = pressedLetters;
 
-if (guessesLeft !== 0 ) {
-	//logic for user key press
-	document.onkeypress = function(e){
-		//user key press
-		var pressed;
-		pressed = e.key;
-		console.log(pressed);
-		document.getElementById("pressed").innerHTML = pressed + (", ");
-	}	
+	//keeping track
+	var wins = 0;
+	var losses = 0;
+	var guessesLeft = 9;
+
+	if (guessesLeft !== 0 ) {
 		
-	
-	if (pressed === pcChoice){
-		wins++;
-		console.log(wins);
-		document.getElementById("wins").innerHTML = wins;
-	}
-	else{
-		losses++;
-		guessesLeft--;
-		console.log(losses);
-		console.log(guessesLeft);
-		document.getElementById("losses").innerHTML = losses;
-		document.getElementById("guessesLeft").innerHTML = guessesLeft;
-	}
-}
-else {
-	
-}
+		if (pressed === pcChoice){
+			wins++;
+			// console.log(wins);
+			document.getElementById("wins").innerHTML = wins;
+		}
+		else{
+			losses++;
+			guessesLeft--;
+			console.log(losses);
+			console.log(guessesLeft);
+			document.getElementById("losses").innerHTML = losses;
+			document.getElementById("guessesLeft").innerHTML = guessesLeft;
+			
+		}
 
+		pcChoice = letters[Math.floor(Math.random()* letters.length)];
+		console.log(pcChoice);
+		pressedLetters.push(pressed);
+	}
+	else {
+		reset();
+		}
+};
 
 
 
