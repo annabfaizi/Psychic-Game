86ee3a49afe8c00a58f4a31790b7a8196566b4aa
Anna B Faizi <annanoor.b@gmail.com>
2017-04-29 01:58:04 -0700

    updated html and js files

---
+++ game.js
@@ -0,0 +1,12 @@
+//set of letters
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//user key press
+var userKeyPress;
+
+var wins = 0;
+var losses = 0;
+var guesses = 10;
+
+var noMatch = [];
+var match = [];
