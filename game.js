f447eca6abcbb20e8c94974dc63376f49a5cd582
Anna B Faizi <annanoor.b@gmail.com>
2017-04-29 02:32:51 -0700

    updated js files

--- game.js
+++ game.js
@@ -1,12 +1,33 @@
 //set of letters
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//user key press
-var userKeyPress;
+//pc letter selection
+var pcChoice = letters.random();
 
+//keeping track
 var wins = 0;
 var losses = 0;
 var guesses = 10;
 
-var noMatch = [];
-var match = [];
+//logic for user key press
+document.onkeypress = function(e){
+	//user key press
+	var pressed;
+	
+	pressed = e.key;
+
+	if (guesses > 0 && pressed !== pcChoice){
+		losses--;
+	}
+	else {
+		wins++;
+	}
+}
+
+
+
+
+
+//comparing letters
+var match = compare(pressed, pcChoice);
+
