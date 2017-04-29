25421b7d336e315af727f9e0dc5036e47bf2dc3d
Anna B Faizi <annanoor.b@gmail.com>
2017-04-29 12:53:54 -0700

    updated js files

--- game.js
+++ game.js
@@ -1,50 +1,66 @@
+$( document ).ready(function() {
+    console.log( "ready!" ); //ready check
+
+
 //set of letters
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
 //pc letter selection
 var pcChoice = letters[Math.floor(Math.random()* letters.length)];
-console.log(pcChoice);
+	console.log(pcChoice);
 
 //keeping track
 var wins = 0;
 var losses = 0;
-var guesses = 9;
 var guessesLeft = 9;
 
-//resets everything
-var reset = function() {
-	guesses = 9;
-	guessesLeft = 9;
-	guessedLetters = [];
-
-	updateguessesLeft();
-	updatepressed();
-
-}
-
-//logic for user key press
-document.onkeypress = function(e){
-	//user key press
-	var pressed;
+if (guessesLeft !== 0 ) {
+	//logic for user key press
+	document.onkeypress = function(e){
+		//user key press
+		var pressed;
+		pressed = e.key;
+		console.log(pressed);
+		document.getElementById("pressed").innerHTML = pressed + (", ");
+	}	
+		
 	
-	pressed = e.key;
-	//comparing letters
-	var compare = function(pressed, pcChoice){
-		if(pressed===pcChoice){
-			wins++;
-		}
-
-		else if (guesses === 0){
-			reset();
-		}
-
-		else {
-			losses++;
-		}
+	if (pressed === pcChoice){
+		wins++;
+		console.log(wins);
+		document.getElementById("wins").innerHTML = wins;
+	}
+	else{
+		losses++;
+		guessesLeft--;
+		console.log(losses);
+		console.log(guessesLeft);
+		document.getElementById("losses").innerHTML = losses;
+		document.getElementById("guessesLeft").innerHTML = guessesLeft;
 	}
 }
+else {
+	
+}
 
 
+
+
+	// //comparing letters
+	// var compare = function(pressed, pcChoice){
+	// 	if(pressed===pcChoice){
+	// 		wins++;
+	// 	}
+
+	// 	else if (guesses === 0){
+	// 		reset();
+	// 	}
+
+	// 	else {
+	// 		losses++;
+	// 	}
+	// }
+})
 
 
 
